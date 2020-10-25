58ccb142c70b46ebba57d5e4a5c92906730530b0
Luke Rigby <1016498+its-rigs@users.noreply.github.com>
2020-10-25 19:16:15 +1300

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-acb96471af32e2ccbc9d.js"
   },
   {
-    "url": "app-7c33e95c40298c2d43cf.js"
+    "url": "app-8bc176d5f807a1ed78f7.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6c38ea8c3cb1448614939af6cba7e624"
+    "revision": "ea42baa96fcbd6c56ba6f1998d4b1f8c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
@@ -48,14 +48,14 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "46c393796950b9595888277899f5d581"
+    "revision": "302b62c4d7b72c4b66a4a39e5a98b8f9"
   },
   {
     "url": "polyfill-72546b9ca477f57c2f2e.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "2af1ce019d3f1570b0b6891cf3a61791"
+    "revision": "f50ae366eba2086d9056cfc238638b09"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -147,7 +147,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/recipease/app-7c33e95c40298c2d43cf.js`))) {
+  if (!resources || !(await caches.match(`/recipease/app-8bc176d5f807a1ed78f7.js`))) {
     return await fetch(event.request)
   }
 
