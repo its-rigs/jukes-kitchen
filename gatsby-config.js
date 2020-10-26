4a1d6c233e845f9fe68bd34028e5766a58044c39
Luke Rigby <1016498+its-rigs@users.noreply.github.com>
2020-10-26 16:11:39 +1300

    Added manifest and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,24 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Juke's Kitchen`,
+        short_name: `Juke's Kitchen`,
+        start_url: `/`,
+        background_color: `#FFF`,
+        theme_color: `#294C89`,
+        display: `standalone`,
+        icon: `assets/icon.png`,
+        icon_options: {
+          purpose: `any maskable`,
+        },
+        legacy: false
+      },
+    },
+    `gatsby-plugin-offline`
+  ],
   pathPrefix: "/recipease",
-  plugins: [],
 }
